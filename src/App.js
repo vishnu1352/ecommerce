542b722a12e0796aa5ede1a79680627d0049f9ac
Vishnu vardhan <vishnuvardhan18100@gmail.com>
2024-05-27 21:16:38 +0530

    new files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+ 
+  return (
+    <>
+      <Header />
+      
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/Header.scss'
+
+const Header = () => {
+  return (
+    <>
+      <div>
+        <p className="m-0 p-3 text-center fs-600">Customized Keychains</p>
+      </div> 
+    </>
+  )
+}
+
+export default Header
